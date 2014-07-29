3eef7e2fde82515a853358dba467b9f3f52f3dd5
Meredith Myers <meredith.myers@dac.us>
2014-07-29 13:15:01 -0400

    Add basic bold colors

---
+++ colors.js
@@ -0,0 +1,11 @@
+/* Colors are from FlatUI http://designmodo.github.io/Flat-UI/ */
+var mlvlColors = [
+  '#16A085', // green sea
+  '#F39C12', // orange
+  '#27AE60', // nephritis
+  '#D35400', // pumpkin
+  '#2980B9', // belize hole
+  '#C0392B', // pomegranate
+  '#8E44AD', // wisteria
+  '#2C3E50'  // midnight blue
+];
