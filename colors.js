45e03fb3d305fddf8637ff066e4f88605d8b3951
meredithmmyers <myers.mm89@gmail.com>
2014-08-01 10:25:37 -0400

    add all colors

--- colors.js
+++ colors.js
@@ -12,5 +12,24 @@ var mlvlColors = {
       '#E67E22', // carrot
       '#16A085', // green sea
     ]
-
+    'turquoise': '#1abc9c',
+    'emerald': '#2ecc71',
+    'green_sea': '#16a085',
+    'nephritis': '#27ae60',
+    'peter_river': '#3498db',
+    'amethyst': '#9b59b6',
+    'belize_hole': '#2980b9',
+    'wisteria': '#8e44ad',
+    'wet_asphalt': '#34495e',
+    'sunflower': '#f1c40f',
+    'midnight_blue': '#2C3E50',
+    'orange': '#f39c12',
+    'carrot': '#E67E22',
+    'alizarin': '#e74c3c',
+    'pumpkin': '#D35400',
+    'pomegranate' : '#C0392B',
+    'clouds': '#ecf0f1',
+    'concrete': '#95a5a6',
+    'silver': '#bdc3c7',
+    'asbetos': '#7f8c8d'
 }
