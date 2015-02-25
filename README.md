27e4722af863bfde579bb5ddbeef04379967db50
meredithmmyers <myers.mm89@gmail.com>
2015-02-25 13:44:57 -0500

    Update colors and readme

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
 colors
 ======
 
-Flat UI based color scheme for visualizations
+Flat UI based color scheme for MLVL visualizations.
+
+Includes:
+
+- ```highContrast```
+- ```vis```
+- ```blueShades```
+- ```yellowShades```
+- ```greenShades```
+- ```orangeShades```
+- ```purpleShades```
+- [FlatUI](http://flatuicolors.co/) colors, by name.
+
+Vis colors and shades:
+
+![ScreenShot](/mlvl-vis-colors.png)
--- colors.js
+++ colors.js
@@ -1,16 +1,77 @@
 /* Colors are from FlatUI http://designmodo.github.io/Flat-UI/ */
 var mlvlColors = {
 
-  'highContrast' :
-    [
-      '#2980B9', // belize hole
-      '#16A085', // green sea
-      '#8E44AD', // wisteria
-      '#E67E22', // carrot
-      '#2C3E50', // midnight blue
-      '#27AE60', // nephritis
-      '#D35400', // pumpkin
-      '#C0392B', // pomegranate
+    'highContrast': [
+        '#2980B9', // belize hole
+        '#16A085', // green sea
+        '#8E44AD', // wisteria
+        '#E67E22', // carrot
+        '#2C3E50', // midnight blue
+        '#27AE60', // nephritis
+        '#D35400', // pumpkin
+        '#C0392B', // pomegranate
+    ],
+    'vis': [
+        '#FCF3CF',
+        '#F9E79F',
+        '#F7DC6F',
+        '#F4D03F',
+        '#F1C40F',
+        '#1A4C6E',
+        '#2772A4',
+        '#3498DB',
+        '#5DADE2',
+        '#85C1E9',
+        '#A3E4D7',
+        '#76D7C4',
+        '#48C9B0',
+        '#1ABC9C',
+        '#148D75',
+        '#AD5F1A',
+        '#E67E22',
+        '#EB984E',
+        '#F0B27A',
+        '#F5CBA7',
+        '#D7BDE2',
+        '#C39BD3',
+        '#AF7AC5',
+        '#9B59B6',
+        '#744389'
+    ],
+    'blueShades': [ // based on peter river
+        '#FCF3CF',
+        '#F9E79F',
+        '#F7DC6F',
+        '#F4D03F',
+        '#F1C40F'
+    ],
+    'yellowShades': [ // based on sunflower
+        '#1A4C6E',
+        '#2772A4',
+        '#3498DB',
+        '#5DADE2',
+        '#85C1E9'
+    ],
+    'turquoiseShades': [ // based on turquoise
+        '#A3E4D7',
+        '#76D7C4',
+        '#48C9B0',
+        '#1ABC9C',
+        '#148D75'
+    ],
+    'orangeShades': [ // based on carrot
+        '#AD5F1A',
+        '#E67E22',
+        '#EB984E',
+        '#F0B27A',
+        '#F5CBA7'
+    ],
+    'purpleShades': [ // based on amethyst
+        '#D7BDE2',
+        '#C39BD3',
+        '#AF7AC5',
+        '#9B59B6',
+        '#744389'
     ],
     'turquoise': '#1abc9c',
     'emerald': '#2ecc71',
@@ -27,7 +88,7 @@ var mlvlColors = {
     'carrot': '#E67E22',
     'alizarin': '#e74c3c',
     'pumpkin': '#D35400',
-    'pomegranate' : '#C0392B',
+    'pomegranate': '#C0392B',
     'clouds': '#ecf0f1',
     'concrete': '#95a5a6',
     'silver': '#bdc3c7',
